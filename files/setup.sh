609f7020c57a0c4dca95f2c96e46ade8d4a2aecd
Peter B. (ArkThis) <pb@das-werkstatt.com>
2025-09-18 09:21:58 +0000

    Replaced pause with a function

--- files/setup.sh
+++ files/setup.sh
@@ -2,25 +2,35 @@
 # Make sure you have the hosts set as your ansible inventory?
 # /etc/ansible/hosts
 
-PAUSE="read -p 'press return...'"
-
-# Fetch the role from upstream:
-if [ ! -d "roles" ]; then
-    ansible-galaxy install -f -p roles -r requirements.yml
-    eval $PAUSE
-fi
-
+# ----------------------------------------------
 ANSIBLE_USER="artefactual"
 ANSIBLE_KEY="$HOME/.ssh/id_rsa"
 ANSIBLE_SUDO="artefactual.sudo"
 ANSIBLE_SUDO_ETC="/etc/sudoers.d/$ANSIBLE_USER"
-ATOM_EDIT="atom-edit.example.com"
-ATOM_RO="atom-ro.example.com"
+
+DOMAIN="example.com"
+ATOM_EDIT_HOST="atom-edit.$DOMAIN"
+ATOM_RO_HOST="atom-ro.allhau.$DOMAIN"
+# ----------------------------------------------
+
+
+function pause
+{
+    read -p 'press return...'
+}
+
+
+# Fetch the role from upstream:
+if [ ! -d "roles" ]; then
+    ansible-galaxy install -f -p roles -r requirements.yml
+    pause
+fi
+
 
 if [ ! -s "$ANSIBLE_SUDO_ETC" ]; then
     echo "Allowing '$ANSIBLE_USER' to sudo without password..."
     sudo cp -av $ANSIBLE_SUDO $ANSIBLE_SUDO_ETC
-    eval $PAUSE
+    pause
 fi
 
 
@@ -30,13 +40,13 @@ if [ ! -s $ANSIBLE_KEY ]; then
     ssh-keygen -t rsa -f $ANSIBLE_KEY
     eval $(ssh-agent)   # From: https://www.cyberciti.biz/faq/how-to-use-ssh-agent-for-authentication-on-linux-unix/
     ssh-add $ANSIBLE_KEY
-    eval $PAUSE
+    pause
 fi
 
 # TODO: Create user automatically.
 adduser $ANSIBLE_USER
-ssh-copy-id $ANSIBLE_USER@atom-edit.example.com
-#ssh-copy-id $ANSIBLE_USER@atom-ro.example.com
+ssh-copy-id $ANSIBLE_USER@$ATOM_EDIT_HOST_
+ssh-copy-id $ANSIBLE_USER@$ATOM_RO_HOST
 
 echo ""
 echo "List and ping hosts for line-check..."
@@ -44,7 +54,7 @@ echo ""
 
 ansible all --list-hosts
 ansible all -m ping
-eval $PAUSE
+pause
 
 
 echo ""
@@ -52,5 +62,5 @@ echo "Run the playbook..."
 echo ""
 
 ansible-playbook mgt.replication-aph-role.yml -e atom_replication_ansible_remote_server=atom-edit-server -t install-replication
-eval $PAUSE
+pause
 
